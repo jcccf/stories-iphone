6b0f34bbe7731a3af5db4500a6e0b25d91c90c55
Justin Cheng <jccccf@gmail.com>
2012-05-07 21:59:12 -0400

    Organization name fix.

--- Storeys/STSingleStoryViewController.m
+++ Storeys/STSingleStoryViewController.m
@@ -3,7 +3,7 @@
 //  Storeys
 //
 //  Created by Justin Cheng on 5/7/12.
-//  Copyright (c) 2012 Cornell University. All rights reserved.
+//  Copyright (c) 2012 colorforest. All rights reserved.
 //
 
 #import "STSingleStoryViewController.h"
